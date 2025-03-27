1cc5997e62ef1e4349d599349ae8b428dc69df87
ankush1038 <ankusharma1029@gmail.com>
2025-03-27 21:08:07 +0530

    [Ankush] Add: Abstract base class for course types constructed

---
+++ src/com/bridgelabz/courseManagement/CourseType.java
@@ -0,0 +1,58 @@
+package com.bridgelabz.courseManagement;
+
+public abstract class CourseType {
+    protected String courseName;
+    protected String instructor;
+
+    public CourseType(String courseName, String instructor) {
+        this.courseName = courseName;
+        this.instructor = instructor;
+    }
+
+    public abstract void displayCourseDetails();
+}
+
+
+class ExamCourse extends CourseType {
+    private int examWeightage;
+
+    public ExamCourse(String courseName, String instructor, int examWeightage) {
+        super(courseName, instructor);
+        this.examWeightage = examWeightage;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Exam-Based Course: " + courseName + " | Instructor: " + instructor + " | Exam Weightage: " + examWeightage + "%");
+    }
+}
+
+
+class AssignmentCourse extends CourseType {
+    private int totalAssignments;
+
+    public AssignmentCourse(String courseName, String instructor, int totalAssignments) {
+        super(courseName, instructor);
+        this.totalAssignments = totalAssignments;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Assignment-Based Course: " + courseName + " | Instructor: " + instructor + " | Total Assignments: " + totalAssignments);
+    }
+}
+
+
+class ResearchCourse extends CourseType {
+    private String researchTopic;
+
+    public ResearchCourse(String courseName, String instructor, String researchTopic) {
+        super(courseName, instructor);
+        this.researchTopic = researchTopic;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Research-Based Course: " + courseName + " | Instructor: " + instructor + " | Research Topic: " + researchTopic);
+    }
+}
